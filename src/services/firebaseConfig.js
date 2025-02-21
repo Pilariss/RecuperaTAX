89af30359192dea730f2d8a71df0186f5879afd0
Larissa Pires <larissadeoliveirapires99@gmail.com>
2025-02-20 22:08:30 -0300

    ajuste secret

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD2h16HT2SLKAh303k9ntBjwFZ5PyxnETw",
-  authDomain: "registration-system-203c8.firebaseapp.com",
-  projectId: "registration-system-203c8",
-  storageBucket: "registration-system-203c8.firebasestorage.app",
-  messagingSenderId: "81750218319",
-  appId: "1:81750218319:web:956ede6567646ef088fc3a",
-  measurementId: "G-XW5TD1HP7X"
-};
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 
